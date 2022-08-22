ee98eea4ac0b48726c391d460dba21175ed72876
Giuseppe Pallazini <Pepepepito1883@gmail.com>
2022-08-22 09:44:19 -0300

    LandingPage

--- src/pages/LandingPage/index.js
+++ src/pages/LandingPage/index.js
@@ -5,29 +5,32 @@ export default function index(){
 
     return(
         <main>
-            <section className="f1-landingpage">
+            <section className="f1">
                 <div className='f1-cabecalho'>
 
                     <div className='f1-cab1'>
                         <p className='f1-cab1-dc1'>Lista de Desejos</p>
-                        <img className='f1-cab1-img1' src="./../../assets/imgs/sacola-listadedesejos.png" alt='' />
-                        <img className='f1-cab1-img2' src="./../../assets/imgs/carrinho.png" alt='' />
+                        <img className='f1-cab1-img1' src="./../../assets/imgs/sacola (1).svg" alt='' />
                     </div>
 
                     <div className='f1-cab2'>
+                            <div className='f1-cab2-0-5'>
                             <img className='f1-cab2-img' src='./../../assets/imgs/logomodellepng.png' alt=''/>
+                            </div>
                             <hr/>
 
                                 <div className='f1-cab2-1'>
-                                    <p>Novidades</p>
-                                    <p>Mulher</p>
-                                    <p>Homem</p>
-                                    <p>Mundo Modelle</p>
+                                    <p className='f1-cab2-1-1'>Novidades</p>
+                                    <p className='f1-cab2-1-2'>Mulher</p>
+                                    <p className='f1-cab2-1-3'>Homem</p>
+                                    <p className='f1-cab2-1-4'>Mundo Modelle</p>
                                 </div>
                     </div>
 
+
                     <div className='f1-cab3'>
-                        <p>Login</p>
+                        <img className='f1-cab1-img2' src="./../../assets/imgs/handbag (1) 1.svg" alt='' />
+                        <p className='f1-cab3-login'>Login</p>
                     </div>
                 </div>
 
@@ -35,8 +38,21 @@ export default function index(){
                     <h1 className='f1-desc1-dc1'>Nova</h1>
                     <div className='f1-desc1-1'>
                         <h1 className='f1-desc1-dc1'>Coleção de Inverno</h1>
-                        <img className='f1-desc1-img1' src='./../../assets/imgs/neve.png' alt=''/>
+                        <img className='f1-desc1-img1' src='./../../assets/imgs/Vector.svg' alt=''/>
                     </div>
+                </div>
+
+            </section>
+
+
+            <section className='f2'>
+                
+
+                <div className='f2-cab1'> 
+                    <p className='f2-cab1-novidades'> Novidades para elas </p>
+                    <p className='f2-cab1-colecao'> Nova coleção Modelle Vuitton </p>
+                    <div className='f2-cab1-lancamento'> Compre os lançamentos</div>
+                    <p className='f2-cab1-data'> * Até 03/23</p>
                 </div>
 
             </section>
